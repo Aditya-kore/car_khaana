a91e3cbe0663a33e0fd85dab0a4f22f7533ae22f
Aditya-kore <66473307+Aditya-kore@users.noreply.github.com>
2021-11-29 14:31:37 +0530

    stable

--- lib/widgets/account_tab.dart
+++ lib/widgets/account_tab.dart
@@ -1,22 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
 
-class AccountTab extends StatefulWidget {
-  @override
-  _AccountTabState createState() => _AccountTabState();
-}
 
-class _AccountTabState extends State<AccountTab> {
+
+class AccountTab extends StatelessWidget {
 
   Material tabItems(IconData icon,String heading, int color) {
     return Material(
       color: Colors.white,
-      elevation: 14,
+      elevation: 14.0,
       shadowColor: Colors.black38,
-      borderRadius: BorderRadius.circular(24),
+      borderRadius: BorderRadius.circular(24.0),
       child: Center(
         child: Padding(
-          padding: EdgeInsets.all(8),
+          padding: EdgeInsets.all(8.0),
           child: Row(
             mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
@@ -27,16 +24,16 @@ class _AccountTabState extends State<AccountTab> {
                   Center(
                     child: Padding(
                       padding: const EdgeInsets.all(8.0),
-                      child: Text(heading,style: TextStyle(color: Colors.blue, //new Color(color)
+                      child: Text(heading,style: TextStyle(color: Colors.blue.shade700,
                       fontSize: 20.0)),
                     ),
                   ),
                   //icon
                   Material(
-                    color: Colors.blue, //new Color(color)
-                    borderRadius: BorderRadius.circular(24),
+                    color: Colors.blue.shade700,
+                    borderRadius: BorderRadius.circular(24.0),
                     child: Padding(
-                      padding: EdgeInsets.all(16),
+                      padding: EdgeInsets.all(16.0),
                       child: Icon(icon,color: Colors.white,
                       size:30,)
                       ),
@@ -52,29 +49,41 @@ class _AccountTabState extends State<AccountTab> {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: StaggeredGridView.count(
-        crossAxisCount: 2,
-        crossAxisSpacing: 12.0,
-        mainAxisSpacing: 12.0,
-        padding: EdgeInsets.symmetric(horizontal: 16,vertical: 8),
-        children: <Widget>[
-          tabItems(Icons.graphic_eq,"Total Views",0000),
-          tabItems(Icons.bookmark,"Bookmark",0000),
-          tabItems(Icons.notifications,"Notifications",0000),
-          tabItems(Icons.attach_email,"Email",0000),
-          tabItems(Icons.settings,"Settings",0000),
-          tabItems(Icons.message,"Messages",0000),
-        ],
-        staggeredTiles: [
-          StaggeredTile.extent(1, 130),
-          StaggeredTile.extent(1, 130),
-          StaggeredTile.extent(1, 130),
-          StaggeredTile.extent(1, 130),
-          StaggeredTile.extent(1, 130),
-          StaggeredTile.extent(1, 130),
-        ],
-      )
+    var dWidth = MediaQuery.of(context).size.width;
+    var dHeight = MediaQuery.of(context).size.height;
+
+    var dPadding = MediaQuery.of(context).padding;
+    var dSafeHeight = dHeight - dPadding.top - dPadding.bottom;
+
+    return Scaffold(
+        body: Column(
+          children: [
+            Expanded(
+              child: StaggeredGridView.count(
+                crossAxisCount: 2,
+                crossAxisSpacing: 20.0,
+                mainAxisSpacing: 20.0,
+                padding: EdgeInsets.symmetric(horizontal: 16,vertical: 8),
+                children: <Widget>[
+                  tabItems(Icons.graphic_eq,"Total Views",0000),
+                  tabItems(Icons.bookmark,"Bookmark",0000),
+                  tabItems(Icons.notifications,"Notifications",0000),
+                  tabItems(Icons.attach_email,"Email",0000),
+                  tabItems(Icons.settings,"Settings",0000),
+                  tabItems(Icons.message,"Messages",0000),
+                ],
+                staggeredTiles: [
+                  StaggeredTile.extent(1, dHeight*0.22),
+                  StaggeredTile.extent(1, dHeight*0.22),
+                  StaggeredTile.extent(1, dHeight*0.22),
+                  StaggeredTile.extent(1, dHeight*0.22),
+                  StaggeredTile.extent(1, dHeight*0.22),
+                  StaggeredTile.extent(1, dHeight*0.22),
+                ],
+              ),
+            ),
+          ],
+        ),
     );
   }
 }
